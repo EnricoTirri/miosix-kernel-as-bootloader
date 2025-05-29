4ad88caf41550ebb769c791c3b45eef02856b895
enti <enrico.tirri@mail.polimi.it>
2025-05-29 18:28:05 +0200

    Cleaned main

--- main.cpp
+++ main.cpp
@@ -3,16 +3,9 @@
 #include "miosix.h"
 
 #include "as_bootloader/bl_manager.h"
-#include "filesystem/file_access.h"
 
 using namespace std;
 using namespace miosix;
-
-void exit_bl()
-{
-    printf("===== Bootloader Exiting : FAIL ======\n");
-    exit(EXIT_FAILURE);
-}
 
 int main()
 {
@@ -21,5 +14,7 @@ int main()
     BootloaderManager blManager;
     blManager.selectFile().loadSelectedFile().boot();
 
-    exit_bl(); // This point should never be reached
+    // This point should never be reached
+    printf("===== Bootloader Exiting : FAIL ======\n");
+    exit(EXIT_FAILURE);
 }
